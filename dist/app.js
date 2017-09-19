faf4ac24db6208541ff13ed831d98ec71a7cbdc8
Joshua Scott <jscott313@gmail.com>
2017-09-19 21:07:44 +0300

    Setting time functionality working

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,20 @@
-"use strict";
+'use strict';
+
+// const breakLength = document.querySelector('.break .time');
+// const sessionLength = document.querySelector('.session .time');
+const buttons = document.querySelectorAll('[data-change]');
+const times = {
+  break: document.querySelector('.break .time'),
+  session: document.querySelector('.session .time')
+};
+
+function setTime(e) {
+  e.preventDefault();
+  const { change, which } = this.dataset;
+  let newTime = parseInt(times[which].textContent) + parseInt(change);
+  if (newTime < 0) newTime = 0;
+  times[which].textContent = newTime;
+}
+
+buttons.forEach(btn => btn.addEventListener('click', setTime));
 //# sourceMappingURL=app.js.map
